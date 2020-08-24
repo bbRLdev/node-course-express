19431fbcdc1f05a7d6b5188e403731aafc3d729e
bbRLdev <michael.labarca@utexas.edu>
2020-08-24 17:26:35 -0500

    Implemented Model View Controller Pattern

--- routes/shop.js
+++ routes/shop.js
@@ -1,22 +1,9 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../utilities/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    products: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsController.getShopProducts);
 
 module.exports = router;
