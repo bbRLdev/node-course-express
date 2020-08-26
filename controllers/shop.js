38e4e967d5dd4a6285574d754572321c8c037f31
bbRLdev <michael.labarca@utexas.edu>
2020-08-25 20:12:10 -0500

    Added product/cart editing/deleting functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -6,6 +8,17 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: 'All Products',
       path: '/products'
+    });
+  });
+};
+
+exports.getProductById = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.fetchProductById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products',
     });
   });
 };
@@ -21,11 +34,40 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for(product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if(cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts,
+      });
+    });
   });
+
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.fetchProductById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.fetchProductById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
